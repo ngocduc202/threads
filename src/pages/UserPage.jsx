0cae8d7f40d054e57068fab57c01bb4facad01b1
ngocduc202 <ngocduc1542002@gmail.com>
2024-09-10 14:17:27 +0700

    create userPage

--- src/pages/UserPage.jsx
+++ src/pages/UserPage.jsx
@@ -1,77 +1,17 @@
 import React from 'react'
 import UserHeader from '../components/UserHeader'
-import { Avatar, Box, Flex, Link, Menu, MenuButton, MenuItem, MenuList, Portal, Text, useToast, VStack } from '@chakra-ui/react'
-import { BsInstagram } from 'react-icons/bs'
-import { CgMoreO } from 'react-icons/cg'
+import UserPost from '../components/UserPost'
+
 
 const UserPage = () => {
-  const toast = useToast()
-  const copyUrl = () => {
-    const currentUrl = window.location.href
-    navigator.clipboard.writeText(currentUrl).then(() => {
-      toast({
-        description: "Profile URL copied to clipboard",
-        status: 'success',
-        duration: 3000,
-        isClosable: true,
-      })
-    })
-  }
 
   return (
-    <VStack gap={4} align={"start"}>
-      <Flex justifyContent={"space-between"} w={"full"}>
-        <Box>
-          <Text fontSize={"2xl"} fontWeight={"bold"}>Mark Zuckerberg</Text>
-          <Flex gap={2} alignItems={"center"}>
-            <Text fontSize={"sm"}>marzuck@fb.com</Text>
-            <Text fontSize={"xs"} bg={"gray.dark"} color={"gray.light"} p={1} borderRadius={"full"} >threads.net</Text>
-          </Flex>
-        </Box>
-        <Box>
-          <Avatar
-            name='Mark'
-            src='/zuck-avatar.png'
-            size={"xl"}
-          />
-        </Box>
-      </Flex>
-      <Text>
-        CO-founder , CEO and CTO of Facebook.
-      </Text>
-      <Flex w={"full"} justifyContent={"space-between"}>
-        <Flex gap={2} alignItems={"center"}>
-          <Text fontSize={"sm"} color={"gray.light"}>3.2k followers</Text>
-          <Box w='1' h='1' bg={"gray.light"} borderRadius={"full"}></Box>
-          <Link color={"gray.light"}>Instagram</Link>
-        </Flex>
-        <Flex>
-          <Box className='icon-container'>
-            <BsInstagram size={24} cursor={"pointer"} />
-          </Box>
-          <Box className='icon-container'>
-            <Menu>
-              <MenuButton>
-                <CgMoreO size={24} cursor={"pointer"} />
-              </MenuButton>
-              <Portal>
-                <MenuList bg={"gray.dark"}>
-                  <MenuItem bg={"gray.dark"} onClick={copyUrl}>Copy Link</MenuItem>
-                </MenuList>
-              </Portal>
-            </Menu>
-          </Box>
-        </Flex>
-      </Flex>
-      <Flex w={"full"}>
-        <Flex flex={1} borderBottom={"1.5px solid white"} justifyContent={"center"} pb={3} cursor={"pointer"} >
-          <Text fontWeight={"bold"}>Threads</Text>
-        </Flex>
-        <Flex flex={1} borderBottom={"1px solid gray"} justifyContent={"center"} color={"gray.light"} pb={3} cursor={"pointer"} >
-          <Text fontWeight={"bold"}>Replies</Text>
-        </Flex>
-      </Flex>
-    </VStack>
+    <>
+      <UserHeader />
+      <UserPost likes={1200} replies={412} postImg="/post1.png" postTitle="Mark Zuckerberg" />
+      <UserPost likes={1220} replies={415} postImg="/post2.png" postTitle="New York" />
+      <UserPost likes={900} replies={312} postImg="/post3.png" postTitle="Leslie Knope" />
+    </>
   )
 }
 
