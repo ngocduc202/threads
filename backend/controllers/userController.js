e023d7e8245c4b072857f18394221d1495a0216e
ngocduc202 <ngocduc1542002@gmail.com>
2024-09-12 14:35:35 +0700

    complete userController

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,19 @@ import User from "../models/userModel.js"
 import bcrypt from "bcryptjs"
 import generateTokenAndSetCookie from "../utils/helpers/generateTokenAndSetCookie.js"
 
+
+export const getUserProfile = async (req, res) => {
+  const {username} = req.params
+  try {
+    const user = await User.findOne({username}).select("-password").select("-updatedAt")
+    if(!user) return res.status(400).json({message: "User not found"})
+    res.status(200).json({user})
+  } catch (error) {
+    res.status(500).json({message: error.message})
+    console.log("Error in getUserProfile: ", error.message)
+  }
+}
+
 export const signupUser = async (req, res) => {
   try {
     const { name , email , username , password} = req.body
@@ -83,7 +96,7 @@ export const followUnfollowUser = async (req, res) => {
     const userToModify = await User.findById(id)
     const currentUser = await User.findById(req.user._id)
 
-    if(id === req.user._id) {
+    if(id === req.user._id.toString()) {
       return res.status(403).json({ message: "You can't follow/unfollow yourself" })
     }
 
@@ -109,3 +122,38 @@ export const followUnfollowUser = async (req, res) => {
     console.log("Error in followUnfollowUser: ", error.message)
   }
 }
+
+export const updateUser = async (req, res) => {
+  const {name , email, username , password , profilePic ,bio} = req.body
+  const userId = req.user._id
+  try {
+    let user = await User.findById(userId)
+    if(!user){
+      return res.status(404).json({message: "User not found"})
+    }
+
+    if(req.params.id !== userId.toString()){
+      return res.status(403).json({message: "You can only update your own profile"})
+    }
+
+    if(password){
+      const salt = await bcrypt.genSalt(10)
+      const hashedPassword = await bcrypt.hash(password, salt)
+      user.password = hashedPassword
+    }
+
+    user.name = name || user.name
+    user.email = email || user.email
+    user.username = username || user.username
+    user.profilePic = profilePic || user.profilePic
+    user.bio = bio || user.bio
+
+    user = await user.save()
+
+    res.status(200).json({message: "Profile updated successfully", user})
+  } catch (error) {
+    res.status(500).json({ message: error.message })
+    console.log("Error in updateUser: ", error.message)
+  }
+}
+
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,12 +1,14 @@
 import express from "express"
-import { followUnfollowUser, loginUser, logout, signupUser } from "../controllers/userController.js"
+import { followUnfollowUser, getUserProfile, loginUser, logout, signupUser, updateUser } from "../controllers/userController.js"
 import protectRoute from "../middlewares/protectRoute.js"
 
 const router = express.Router()
 
+router.get("/profile/:username", getUserProfile)
 router.post("/signup", signupUser)
 router.post("/login", loginUser)
 router.post("/logout", logout)
 router.post("/follow/:id", protectRoute , followUnfollowUser)
+router.post("/update/:id", protectRoute , updateUser)
 
 export default router
