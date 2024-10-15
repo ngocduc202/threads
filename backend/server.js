f3363573073469c19cc2ce8ab798e22333301efe
ngocduc202 <ngocduc1542002@gmail.com>
2024-10-16 00:57:06 +0700

    add cron

--- backend/server.js
+++ backend/server.js
@@ -9,10 +9,12 @@ import notificationRoutes from './routes/notificationRoutes.js'
 import {v2 as cloudinary} from 'cloudinary'
 import {app , server} from './socket/socket.js'
 import path from 'path'
+import job from './cron/cron.js'
 
 
 dotenv.config()
 connectDB()
+job.start()
 
 
 const PORT = process.env.PORT || 5000
