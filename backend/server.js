834e8a772ad2d995df9594ed5851ced984ca32fd
ngocduc202 <ngocduc1542002@gmail.com>
2024-10-16 00:41:19 +0700

    deploy

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import messageRoutes from './routes/messageRoutes.js'
 import notificationRoutes from './routes/notificationRoutes.js'
 import {v2 as cloudinary} from 'cloudinary'
 import {app , server} from './socket/socket.js'
+import path from 'path'
 
 
 dotenv.config()
@@ -15,6 +16,7 @@ connectDB()
 
 
 const PORT = process.env.PORT || 5000
+const __dirname = path.resolve()
 
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
@@ -30,6 +32,14 @@ app.use("/api/users" , userRoutes)
 app.use("/api/posts" , postRoutes)
 app.use("/api/messages" , messageRoutes)
 app.use("/api/notifications" , notificationRoutes)
+
+if(process.env.NODE_ENV === 'production'){
+  app.use(express.static(path.join(__dirname , '/frontend/dist')))
+
+  app.get("*" , (req , res) => {
+    res.sendFile(path.resolve(__dirname , 'frontend' , 'dist' , 'index.html'))
+  })
+}
 
 server.listen(PORT , () => {
   console.log(`Server running on port ${PORT}`)
